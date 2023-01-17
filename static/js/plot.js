f0e438f60aa0c00bd8080f47920ef0772586716e
Jovawnte <mofpmgt@gmail.com>
2023-01-17 18:40:58 -0500

    update

--- static/js/plot.js
+++ static/js/plot.js
@@ -3,7 +3,7 @@
 
     let points = [{
       values: [46, 54, 35, 10, 11, 55,],
-      labels: ['Asian Foodball Confederation (AFC)', 'Confederation of African Football (CAF)', 'Confederation of North, Central America and Caribbean Association Football (CONCACAF)', 'South American Football Confederation (CONMEBOL)', 'Oceania Football Confederation (OFC)', 'Union of European Football Associations (UEFA)'],
+      labels: ['Asian Football Confederation (AFC)', 'Confederation of African Football (CAF)', 'Confederation of North, Central America and Caribbean Association Football (CONCACAF)', 'South American Football Confederation (CONMEBOL)', 'Oceania Football Confederation (OFC)', 'Union of European Football Associations (UEFA)'],
       type: "pie"
     }];
 
